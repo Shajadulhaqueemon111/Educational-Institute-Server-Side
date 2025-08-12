9666272d9b2ad3adace026059b3124c8d231b645
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-08-12 22:47:25 +0600

    added a middleware function

--- src/app/interface/errorType.ts
+++ src/app/interface/errorType.ts
@@ -3,7 +3,7 @@ export type TErrorSources = {
   message: string;
 }[];
 
-export type TGenricErrorSources = {
+export type TGenericErrorResponse = {
   statusCode: number;
   message: string;
   errorSources: TErrorSources;
