3a2a1899c2453b1dda39cb77d67b77bca4871bc2
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-08-20 23:09:15 +0600

    add a student modle schema

--- src/app/modules/student/student.modle.ts
+++ src/app/modules/student/student.modle.ts
@@ -1,9 +1,37 @@
-import { Schema } from 'mongoose';
+import { model, Schema } from 'mongoose';
 import { TStuden } from './student.interface';
 
 const StudentSchema = new Schema<TStuden>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   name: {
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  rollNumber: {
+    type: String,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
 });
+
+const StudentModle = model<TStuden>('Student', StudentSchema);
+
+export default StudentModle;
